3c688c999c7bc09dfad55b5295dbc81a557c6f6b
RuoyaUCSD <156246456+RuoyaUCSD@users.noreply.github.com>
2024-03-06 12:55:57 -0800

    Messed things up in a different way

--- ListExamples.java
+++ ListExamples.java
@@ -1,8 +1,5 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-
-import javax.swing.event.ListSelectionEvent;
 
 interface StringChecker { boolean checkString(String s); }
 
@@ -11,23 +8,41 @@ class ListExamples {
   // Returns a new list that has all the elements of the input list for which
   // the StringChecker returns true, and not the elements that return false, in
   // the same order they appeared in the input list;
-  static List<String> filter(List<String> list, StringChecker sc) {
-    for(int i = 0; i < list.size(); i++) {
-      if(!sc.checkString(list.get(i))) {
-        list.remove(i);
-        i--;
+  static ArrayList<String> filter(List<String> list, StringChecker sc) {
+    ArrayList<String> result = new ArrayList<>();
+    for(String s: list) {
+      if(sc.checkString(s)) {
+        result.add(s);
       }
     }
-    return list;
+    return result;
   }
 
 
   // Takes two sorted list of strings (so "a" appears before "b" and so on),
   // and return a new list that has all the strings in both list in sorted order.
-  static List<String> merge(List<String> list1, List<String> list2) {
-    list1.addAll(list2);
-    Collections.sort(list1);
-    return list1;
+  static Object[] merge(List<String> list1, List<String> list2) {
+    ArrayList<String> result = new ArrayList<>();
+    int index1 = 0, index2 = 0;
+    while(index1 < list1.size() && index2 < list2.size()) {
+      if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
+        result.add(list1.get(index1));
+        index1 += 1;
+      }
+      else {
+        result.add(list2.get(index2));
+        index2 += 1;
+      }
+    }
+    while(index1 < list1.size()) {
+      result.add(list1.get(index1));
+      index1 += 1;
+    }
+    while(index2 < list2.size()) {
+      result.add(list2.get(index2));
+      index2 += 1;
+    }
+    return result.toArray();
   }
 
 
